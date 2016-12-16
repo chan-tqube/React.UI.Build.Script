078b867ee7fbfbaa2b64140963eb56b27fc1edaa
Chandresh Rajkumar Manonmani <chan@tqube.com>
2016-12-17 01:13:37 +0530

    FS Modified

--- tasks/fs.js
+++ tasks/fs.js
@@ -8,4 +8,54 @@ export const readFile = (file) => new Promise ((resolve, reject) => {
   fs.readFile(file, 'utf8', (err, data) => (err ? reject(err) : resolve(data)));
 });
 
+export const writeFile = (file, contents) => new Promise((resolve, reject) => {
+  fs.writeFile(file, contents, 'utf8', err => (err ? reject(err) : resolve()));
+});
 
+export const copyFile = (source, target) => new Promise((resolve, reject) => {
+  let cbCalled = false;
+  function done (err) {
+    if (!cbCalled) {
+      cbCalled = true;
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    }
+  }
+
+  const rd = fs.createReadStream(source);
+  rd.on('error', err => done(err));
+  const wr = fs.createWriteStream(target);
+  wr.on('error', err => done(err));
+  wr.on('close', err => done(err));
+  rd.pipe(wr);
+});
+
+export const readDirectory = (pattern, options) => new Promise((resolve, reject) =>
+  glob(pattern, options, (err, result) => (err ? reject(err) : resolve(result)));
+);
+
+export const makeDirectory = (name) => new Promise((resolve, reject) => {
+  mkdirp(name, err => (err ? reject(err) : resolve()));
+});
+
+export const copyDirectory = async (source, target) => {
+  const dirs = await readDir('**/*.*', {
+    cwd: source,
+    nosort: true,
+    dot: true
+  });
+
+  await Promise.all(dirs.map(async dir => {
+    const from = path.resolve(source, dir);
+    const to = path.resolve(target, dir);
+    await makeDirectory(path.dirname(to));
+    await copyFile(from, to);
+  }));
+}
+
+export const cleanDirectory = (pattern, options) => new Promise((resolve, reject) =>
+  rimraf(pattern, { glob: options }, (err, result) => (err ? reject(err) : resolve(result))),
+);
